cb131130e0cf1bddab0577bf73a259eec8d45aaf
David Wang <david2000wang@gmail.com>
2023-02-26 17:35:36 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import moment from 'moment'
 
 function App() {
+  const [numToDos, setNumToDos] = useState(0);
+  const [toDos, setTodos] = useState([[]]);
+  const [isTextEmpty, setIsTextEmpty] = useState(true);
+
+  const date_format = "MMM-DD-YYYY hh:mm:ss A"
+  const [curTime, setCurTime] = useState(moment().format(date_format)) 
+
+  const clickedEnter = () => {
+    setNumToDos(numToDos + 1)
+    setTodos([...toDos, [document.getElementById("App-Text-Field").value, curTime]]);
+
+    document.getElementById('App-Text-Field').value = "";
+    setIsTextEmpty(true);
+    // console.log(toDos)
+  }
+
+  useEffect(() => {
+    const todo_li = toDos.map((todo, index) => <li className={index}>{todo[0]} Added: {todo[1]}</li>)
+    // console.log(todo_li)
+    document.getElementsByClassName('App-Display-ToDos').value = ""
+    // document.getElementsByClassName('App-Display-ToDos').appendChild(todo_li)
+  }, [toDos])
+  // const listToDos = toDos.map((todo, index) => {
+  //     console.log("todo!", todo, index);
+  //     return(<li className={index}>{todo[0]} Added: {todo[1]}</li>)
+  //   }
+  // )
+
+  // useEffect(, [setInterval(() => setCurTime(moment.format(date_format)), 1000)])
+  setInterval(() => {
+    setCurTime(moment().format(date_format))
+  }, 1000)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App-Time'>
+        Current Time: {curTime}
+      </div>
+      <div className='App-New'>
+        <form>
+          <label for="App-Text-Field">Add New ToDo: </label>
+          <input name="App-Text-Field" id="App-Text-Field" type="text" onChange={() => {
+            document.getElementById("App-Text-Field").value == "" ? setIsTextEmpty(true) : setIsTextEmpty(false)
+          }}/>
+          <button name="enter" id="enter" type='button' disabled={isTextEmpty} onClick={clickedEnter}>Enter</button>
+        </form>
+      </div>
+      <div className='App-Display'>
+        <p> Total todos: {numToDos}</p>
+        <ul className='App-Display-ToDos'></ul>
+      </div>
     </div>
   );
 }
